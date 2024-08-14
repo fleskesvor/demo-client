8dcbc34fc3e0ad6087bc21ea91b1b66abff76fc7
fleskesvor <eivindkn@gmail.com>
2024-08-14 13:45:33 +0200

    Debug access token

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -6,29 +6,39 @@ const oidcConfig: UserManagerSettings = {
     authority: "http://localhost:8080",
     client_id: "test-client",
     redirect_uri: "http://localhost:3000/callback",
-    scope: "openid"
+    scope: "user.read"
 }
 
 let userManager: UserManager | null = null;
 
 export const authentication = reactive({
     isAuthenticated: false,
+    accessToken: "",
+    parsedAccessToken: {},
 });
 
 export const auth = {
     install: () => {
         console.debug("Initialize authentication");
-
         userManager = new UserManager(oidcConfig);
     }
+};
+
+const parseToken = (token: string | undefined) => {
+    if (!token) return {};
+    const [ _, payload ] = token.split(".");
+    return JSON.parse(atob(payload));
 };
 
 export const login = () => userManager?.signinRedirect();
 
 export const signinCallback = () => {
     userManager?.signinCallback().then((user: User | void) => {
+        console.debug(`Authenticated user: ${user ? JSON.stringify(user) : null}`)
         if (user) {
             authentication.isAuthenticated = true;
+            authentication.accessToken = user.access_token || "";
+            authentication.parsedAccessToken = parseToken(user.access_token);
             router.push("/")
         }
     });
